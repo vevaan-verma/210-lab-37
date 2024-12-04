71b5464cf29aa6dc8b60123daae0e0388b5d0823
Vevaan <vevaan.s.verma@gmail.com>
2024-12-03 23:06:02 -0800

    Finish part one

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 // COMSC-210 | Lab 37 | Vevaan Verma
 using namespace std;
 #include <iostream>
+#include <string>
+
+/* FUNCTION PROTOTYPES */
+int sum_ascii(string s);
 
 // main() is the entry point of the program and drives the program
 // arguments: none
 // returns: int - the exit code of the program
 int main() {
 
-	char a = 'A';
-	cout << a << endl;
-	cout << (int) a << endl;
-
-	int b = 66;
-	cout << b << endl;
-	cout << (char) b << endl;
-
+	cout << sum_ascii("COMSC-210") << endl;
 	return 0;
+
+}
+
+// sum_ascii() takes a string and returns the sum of the ASCII values of the characters in the string
+// arguments: string s - the string to sum the ASCII values of
+// returns: int - the sum of the ASCII values of the characters in the string
+int sum_ascii(string s) {
+
+	int sum = 0;
+
+	for (int i = 0; i < s.length(); i++)
+		sum += (int) s[i];
+
+	return sum;
 
 }
 
