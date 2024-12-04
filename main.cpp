803e69c83a42b5adf07074cc84dcd566102be3e6
Vevaan <vevaan.s.verma@gmail.com>
2024-12-03 22:58:18 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// COMSC-210 | Lab 37 | Vevaan Verma
+using namespace std;
+#include <iostream>
+
+// main() is the entry point of the program and drives the program
+// arguments: none
+// returns: int - the exit code of the program
+int main() {
+
+	char a = 'A';
+	cout << a << endl;
+	cout << (int) a << endl;
+
+	int b = 66;
+	cout << b << endl;
+	cout << (char) b << endl;
+
+	return 0;
+
+}
+
+/*
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
